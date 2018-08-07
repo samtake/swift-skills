9242d969e999a2bcc97d64548d706126ff87d5b3
Sam <943430195@qq.com>
2018-08-08 01:12:25 +0800

    empty project

---
+++ swiftSkills/swiftSkills/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swiftSkills
+//
+//  Created by huanglongshan on 2018/8/8.
+//  Copyright © 2018年 huanglongshan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
